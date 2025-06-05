b7a76a0fd0eea8151c2d71ae825d4221501a39a5
Carla Jeanpierre <carlatresejeanpierre@gmail.com>
2025-06-04 23:07:50 -0400

    add a function that checks whether the input is a palindrome and clear the value after check

--- palindrome checker/index.js
+++ palindrome checker/index.js
@@ -1,6 +1,18 @@
 const input = document.getElementById("input")
 
+function reverseString(str) {
+    return str.split("").reverse().join("")
+}
+
 function check() {
     const value = input.value
-    alert(value)
+    const reverse = reverseString(value)
+    
+    if (value === reverse) {
+        alert("THIS IS INDEED A PALINDROME")
+    } else {
+        alert("THIS IS NOT A PALINDROME")
+    }
+
+    input.value = ""
 }
